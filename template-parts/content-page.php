e2e0ead8f6c2adbdf09950b66339cc4b45c1452d
Damon Carr <carr.damon@gmail.com>
2016-03-17 21:50:09 +1100

    Add hero image and breadcrumbs code to page template

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -11,9 +11,22 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+		<div class="inner">
+			<div class="hero-image-page">
+				<?php if ( has_post_thumbnail() ) { 
+			    the_post_thumbnail( 'post_masthead_pic' ); 
+				} ?>
+			</div>
 
+			<?php the_title( '<h1 class="entry-title page-title">', '</h1>' ); ?>
+		</div>
+	</header><!-- .entry-header -->
+	<div class="breadcrumbs" typeof="BreadcrumbList" vocab="http://schema.org/">
+    <?php if(function_exists('bcn_display'))
+    {
+        bcn_display();
+    }?>
+	</div>
 	<div class="entry-content">
 		<?php
 			the_content();
