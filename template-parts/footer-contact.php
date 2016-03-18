5c6c1cb4377ebbea5f95080505ea953e9fb3545a
Damon Carr <carr.damon@gmail.com>
2016-03-19 08:22:16 +1100

    Post footer contact area

--- template-parts/footer-contact.php
+++ template-parts/footer-contact.php
@@ -11,6 +11,20 @@
 
 <div class="contact-footer">
 		<div class="inner">
-			mfsdlkfjsdlkfj
+			<h3>Contact Us</h3>
+			<div class="phonenumber">
+				<h4>Telephone</h4>
+				<p>03 9571 2931</p>
+			</div>
+			<div class="school-tour">
+				<h4>School tour</h4>
+				<p>Book a personal school tour with our principal Libby Alessi</p>
+				<a href="#">Book a school tour</a>
+			</div>
+			<div class="enquiry">
+				<h4>Send us an enquiry</h4>
+				<p>Need more informaion on the school?</p>
+				<a href="#">Send enquiry</a>
+			</div>
 		</div>
 </header>
