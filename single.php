af4c00f9eba26d9db6f8e09d9a3c17438cae3370
Damon Carr <carr.damon@gmail.com>
2016-04-26 06:26:47 +1000

    Work on single post template for news stories

--- single.php
+++ single.php
@@ -15,7 +15,7 @@ get_header(); ?>
 		<?php
 		while ( have_posts() ) : the_post();
 
-			get_template_part( 'template-parts/content', get_post_format() );
+			get_template_part( 'template-parts/content', 'single' );
 
 			the_post_navigation();
 
@@ -24,6 +24,8 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
+
+	<?php get_template_part( 'template-parts/footer', 'contact'); ?>
 
 <?php
 get_footer();
