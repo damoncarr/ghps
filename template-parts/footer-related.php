0041c5c200cf2c114dbe51484e5cb9a0785ade2f
Damon Carr <carr.damon@gmail.com>
2016-04-30 21:27:28 +1000

    fix colour and .inner on related content footer

--- template-parts/footer-related.php
+++ template-parts/footer-related.php
@@ -10,22 +10,23 @@
 ?>
 
 <div class="related-footer">
-			
-<?php
+	<div class="inner">
+				
+		<?php
 
-$post_objects = get_field('related_content');
+		$post_objects = get_field('related_content');
 
-if( $post_objects ): ?>
-<h3>Also in this section</h3>
-    <ul>
-    <?php foreach( $post_objects as $post_object): ?>
-        <li>
-            <a href="<?php echo get_permalink($post_object->ID); ?>"><?php echo get_the_title($post_object->ID); ?></a>
-        </li>
-    <?php endforeach; ?>
-    </ul>
-<?php endif;
+		if( $post_objects ): ?>
+		<h3>Also in this section</h3>
+		    <ul>
+		    <?php foreach( $post_objects as $post_object): ?>
+		        <li>
+		            <a href="<?php echo get_permalink($post_object->ID); ?>"><?php echo get_the_title($post_object->ID); ?></a>
+		        </li>
+		    <?php endforeach; ?>
+		    </ul>
+		<?php endif;
 
-?>
-
+		?>
+	</div>
 </div>
